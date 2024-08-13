575c64fbe2e02b01bed86c0aea5a54362c66d51b
msr <msrxse@gmail.com>
2024-08-13 16:53:39 +0100

    Turn right works

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,30 @@
-const turnLeft = (orientation) => {
-  if (orientation === "N") return "W";
-  if (orientation === "W") return "S";
-  if (orientation === "S") return "E";
-  if (orientation === "E") return "N";
+const Compass = ["N", "E", "S", "W"];
+
+const turnRight = (orientation) => {
+  return Compass[(Compass.indexOf(orientation) + 1) % 4];
 };
 
-test("When facing N, turning L should cause us to face W", () => {
-  expect(turnLeft("N")).toBe("W");
-});
-test("When facing W, turning L should cause us to face S", () => {
-  expect(turnLeft("N")).toBe("W");
-});
-test("When facing S, turning L should cause us to face E", () => {
-  expect(turnLeft("S")).toBe("E");
-});
-test("When facing E, turning L should cause us to face N", () => {
-  expect(turnLeft("E")).toBe("N");
-});
+const turnLeft = (orientation) => {
+  return Compass[(Compass.indexOf(orientation) + 3) % 4];
+};
+
+test.each`
+  original | expected | direction
+  ${"N"}   | ${"E"}   | ${"R"}
+  ${"E"}   | ${"S"}   | ${"R"}
+  ${"S"}   | ${"W"}   | ${"R"}
+  ${"W"}   | ${"N"}   | ${"R"}
+  ${"N"}   | ${"W"}   | ${"L"}
+  ${"W"}   | ${"S"}   | ${"L"}
+  ${"S"}   | ${"E"}   | ${"L"}
+  ${"E"}   | ${"N"}   | ${"L"}
+`(
+  "When facing $original, turning $direction should cause us to face $expected",
+  ({ original, expected, direction }) => {
+    if (direction === "R") {
+      expect(turnRight(original)).toBe(expected);
+    } else if (direction === "L") {
+      expect(turnLeft(original)).toBe(expected);
+    }
+  }
+);
